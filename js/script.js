c9d377ae10374a01d32f27ee39649160eb52fafc
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-24 14:44:01 +0100

    file base

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova (facciamo questo in 2 versioni: col ciclo for e col while)
+
